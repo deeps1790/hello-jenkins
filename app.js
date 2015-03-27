6d692b49c04050d6a1b81f1dd24cbda288221087
Rajesh Kumar Singh <rajesh@mobicules.com>
2015-03-27 17:05:17 +0530

    Switch to hello deep

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-    res.send('hello deepu');
+    res.send('hello deep');
 });
 
 app.listen(process.env.PORT || 5000);
