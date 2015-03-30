2bff648d5e0a0b172086bcb602ef2ae1678859bd
Rajesh Kumar Singh <rajesh@mobicules.com>
2015-03-30 15:50:34 +0530

    Change Port Number

--- app.js
+++ app.js
@@ -3,9 +3,9 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-    res.send('hello world');
+    console.log('hello world');
 });
 
-app.listen(process.env.PORT || 8181);
+app.listen(process.env.PORT || 3000);
 
 module.exports = app;
