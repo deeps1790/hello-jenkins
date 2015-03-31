d2bfabae83aedc8341200989a1916895a4f3b5c3
Rajesh Kumar Singh <rajesh@mobicules.com>
2015-03-31 11:57:04 +0530

    bbbdsadassfsf

--- github.php
+++ github.php
@@ -1,19 +1,2 @@
 <?php 
-try
-{
-  $payload = json_decode($_REQUEST['payload']);
-}
-catch(Exception $e)
-{
-  exit(0);
-}
-
-//log the request
-file_put_contents('logs/github.txt', print_r($payload, TRUE), FILE_APPEND);
-
-
-if ($payload->ref === 'refs/heads/master')
-{
-  // path to your site deployment script
-  exec('./build.sh');
-}
+echo shell_exec('sh /vas/www/html/deepu/build.sh');
